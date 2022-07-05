c7c4e8ac7234da03b9fe57b2d174c2616d0a8d6d
Luiz-Lins <luizlins22@gmail.com>
2022-07-05 00:19:35 -0300

    Criado arquivos para deploy no heroku

--- pypro/urls.py
+++ pypro/urls.py
@@ -1,4 +1,4 @@
-"""pypro URL Configuration
+"""pypr URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
