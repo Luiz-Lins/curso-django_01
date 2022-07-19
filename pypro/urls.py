c4d5bada314e211e9ce11e7cba7a434435efce3c
Luiz-Lins <luizlins22@gmail.com>
2022-07-19 00:56:45 -0300

    Configuração video vimeo e integrações simples.

--- pypro/urls.py
+++ pypro/urls.py
@@ -17,10 +17,10 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pypro.base.urls')),
+    path('aperitivos/', include('pypro.aperitivos.urls')),
 ]
 
 if settings.DEBUG:
