ef1a38aecf1b8b4d939bf6e0e8eeba22d48406dd
Luiz-Lins <luizlins22@gmail.com>
2022-09-02 21:27:20 -0300

    Correção Ordered Models em test facade.

--- pypro/modulos/tests/test_facade.py
+++ pypro/modulos/tests/test_facade.py
@@ -7,7 +7,7 @@ from pypro.modulos.models import Modulo
 
 @pytest.fixture
 def modulos(db):
-    return [mommy.make(Modulo, titulo=s) for s in 'Antes, Depois'.split()]
+    return [mommy.make(Modulo, titulo=s) for s in 'Antes Depois'.split()]
 
 
 def test_listar_modulos_ordenados(modulos):
